17b480fb64fe70d2a8ba2a1af573a9a06de76604
michal-owsiak <micowsiak@gmail.com>
2026-03-15 13:01:15 +0100

    convert_to_usd macro created

--- models/marts/whale_alert.sql
+++ models/marts/whale_alert.sql
@@ -8,22 +8,12 @@ with whales as (
     where output_value > 10
     group by output_address
 
-),
-
-latest_price as (
-
-    select
-        price
-    from {{ ref('btc_usd_max') }}
-    where to_date(replace(snapped_at, 'UTC', '')) = current_date()
-
 )
 
 select
     w.output_address,
     w.total_sent,
     w.transaction_count,
-    (lp.price * w.total_sent) as current_value_usd
+    {{ convert_to_usd('w.total_sent') }} as current_value_usd
 from whales w
-cross join latest_price lp
 order by w.total_sent desc
