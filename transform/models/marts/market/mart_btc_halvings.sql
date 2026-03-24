8abb905e41472ad88aa254ab537b135dd36fe59e
michal-owsiak <micowsiak@gmail.com>
2026-03-24 21:50:20 +0100

    mart_btc_halvings model updated

--- transform/models/marts/market/mart_btc_halvings.sql
+++ transform/models/marts/market/mart_btc_halvings.sql
@@ -4,6 +4,15 @@
     )
 }}
 
+with min_historical_data as (
+
+    select 
+        min(open_time) as min_open_time
+    from 
+        {{ ref('mart_btc_price_supertrend_1d') }}
+
+)
+
 select
     halving_number,
     halving_date,
@@ -11,4 +20,10 @@ select
     btc_to,
     is_historical,
     is_projected
-from {{ ref('stg_btc_halvings') }}
+from 
+    {{ ref('stg_btc_halvings') }}
+where 
+    halving_date > (
+        select min_open_time
+        from min_historical_data
+    )
